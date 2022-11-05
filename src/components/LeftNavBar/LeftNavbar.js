080f9ab993d14619e74fb511d37914db180caafc
Alan Jones <alan@hootreading.com>
2022-11-04 20:59:39 -0500

    updated links for icons on menu

--- src/components/LeftNavBar/LeftNavbar.js
+++ src/components/LeftNavBar/LeftNavbar.js
@@ -12,49 +12,60 @@ function LeftNavbar() {
   return (
     <div className={styles.nav_container}>
       <div className={styles.logo}>
-      <img
-              src="../../../logo-large.png"
-              alt="user"
-              className={styles.profile_image}
-            />
+        <img
+          src="../../../logo-large.png"
+          alt="user"
+          className={styles.profile_image}
+        />
       </div>
       <div className={styles.nav_wrapper}>
         <ul className={styles.nav_items}>
           <li className={styles.nav_item}>
-            <FontAwesomeIcon
-              icon={faTachometerAlt}
-              style={{ width: "18px", cursor: "pointer" }}
-            />{" "}
+            <a href="/">
+              <FontAwesomeIcon
+                icon={faTachometerAlt}
+                style={{ width: "18px", cursor: "pointer" }}
+              />{" "}
+            </a>
+
             <a className={styles.nav_item__link} href="/">
               Dashboard
             </a>
           </li>
           <li className={styles.nav_item}>
-            <FontAwesomeIcon
-              icon={faRocket}
-              style={{ width: "18px", cursor: "pointer" }}
-            />{" "}
+            <a href="/coin-system">
+              <FontAwesomeIcon
+                icon={faRocket}
+                style={{ width: "18px", cursor: "pointer" }}
+              />{" "}
+            </a>
             <a className={styles.nav_item__link} href="/coin-system">
               Coin System
             </a>
           </li>
 
           <li className={styles.nav_item}>
-            <FontAwesomeIcon
-              icon={faCog}
-              style={{ width: "18px", cursor: "pointer" }}
-            />{" "}
+            <a href="/settings">
+              <FontAwesomeIcon
+                icon={faCog}
+                style={{ width: "18px", cursor: "pointer" }}
+              />{" "}
+            </a>
+
             <a className={styles.nav_item__link} href="/settings">
               {" "}
               Settings
             </a>
           </li>
           <li className={styles.nav_item}>
-            <FontAwesomeIcon
-              icon={faSignOutAlt}
-              style={{ width: "18px", cursor: "pointer" }}
-            />{" "}
-            <a className={styles.nav_item__link} href="logout">
+            <a href="/logout">
+              <FontAwesomeIcon
+                icon={faSignOutAlt}
+                style={{ width: "18px", cursor: "pointer" }}
+              />{" "}
+            </a>
+
+            <a className={styles.nav_item__link} href="/logout">
               Logout
             </a>
           </li>
